711704d53afe3975422a2586a730d2421fe6ca2d
Jorge Navidad <jorge.espliego@outlook.com>
2024-11-27 17:42:09 +0100

    HerosUseCase

--- PracticaCombineAsync/Data/Network/BaseNetwork/EndPoints.swift
+++ PracticaCombineAsync/Data/Network/BaseNetwork/EndPoints.swift
@@ -3,5 +3,6 @@ import Foundation
 enum EndPoints: String {
     
     case login = "/api/auth/login"
+    case heros = "/api/heros/all"
     
 }
---
+++ PracticaCombineAsync/Domain/Protocols/HerosRepositoryProtocol.swift
@@ -0,0 +1,6 @@
+
+import Foundation
+
+protocol HerosRepositoryProtocol {
+    func getHeros(filter: String) async -> [HerosModel] 
+}

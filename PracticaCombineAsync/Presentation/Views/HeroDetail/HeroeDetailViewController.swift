98d72a2af566f4eec3a96d96374292f96f683f43
Jorge Navidad <jorge.espliego@outlook.com>
2024-11-28 15:27:33 +0100

    commit de seguridad 1 (a hacer test!)

--- PracticaCombineAsync/Presentation/Views/HeroDetail/HeroeDetailViewController.swift
+++ PracticaCombineAsync/Presentation/Views/HeroDetail/HeroeDetailViewController.swift
@@ -3,17 +3,17 @@ import Combine
 
 class HeroeDetailViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    // MARK: - Outlets
+
     @IBOutlet weak var heroPhoto: UIImageView!
     @IBOutlet weak var infoHero: UITextView!
     @IBOutlet weak var transformationsContainer: UICollectionView!
     @IBOutlet weak var heroName: UILabel!
     
-    // MARK: - Properties
+
     private var viewModel: HeroDetailViewModel
     private var suscriptions = Set<AnyCancellable>()
     
-    // MARK: - Init
+
     init(viewModel: HeroDetailViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -23,16 +23,16 @@ class HeroeDetailViewController: UIViewController, UICollectionViewDataSource, U
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
         configureCollectionView()
         bindingUI()
-        fetchTransformations()
+        getTransformations()
     }
     
-    // MARK: - Configure UI
+
     private func configureUI() {
         self.heroName.text = self.viewModel.hero.name
         self.infoHero.text = self.viewModel.hero.description
@@ -46,23 +46,19 @@ class HeroeDetailViewController: UIViewController, UICollectionViewDataSource, U
         transformationsContainer.dataSource = self
         transformationsContainer.delegate = self
         
-        // Registrar la celda personalizada
         transformationsContainer.register(
             UINib(nibName: "HeroeDetailCollectionViewCell", bundle: nil),
             forCellWithReuseIdentifier: "HeroeDetailCollectionViewCell"
         )
         
-        // Configurar layout del UICollectionView
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 100, height: 120) // Ajustar tamaño de las celdas
+        layout.itemSize = CGSize(width: 100, height: 120)
         layout.minimumLineSpacing = 10
         layout.minimumInteritemSpacing = 10
         transformationsContainer.collectionViewLayout = layout
     }
     
-    // MARK: - Bindings
     private func bindingUI() {
-        // Vincular transformaciones del ViewModel al CollectionView
         self.viewModel.$transformations
             .receive(on: DispatchQueue.main)
             .sink { [weak self] _ in
@@ -71,14 +67,12 @@ class HeroeDetailViewController: UIViewController, UICollectionViewDataSource, U
             .store(in: &suscriptions)
     }
     
-    // MARK: - Fetch Data
-    private func fetchTransformations() {
+    private func getTransformations() {
         Task {
             await self.viewModel.getTransformations()
         }
     }
     
-    // MARK: - UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.transformations.count
     }
@@ -93,7 +87,6 @@ class HeroeDetailViewController: UIViewController, UICollectionViewDataSource, U
         return cell
     }
     
-    // MARK: - UICollectionViewDelegate (Opcional)
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let transformation = viewModel.transformations[indexPath.item]
         print("Selected transformation: \(transformation.name)")
